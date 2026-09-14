3ea53cd6d78f613f9a9821f7096244b6af3e41ac
Midnight Cyber Rider <221291534+Dexsam07@users.noreply.github.com>
2026-09-14 13:15:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ module.exports = {
     connectionNotifyEnabled: true,
     developerName: 'Dex Shyam Chaudhari',
     githubUrl: 'https://github.com/Dexsam07/As-zara-mini',
-    newsletterJid: '120363406449026172@newsletter', // Newsletter JID for menu forwarding
+    newsletterJid: '', // Newsletter JID for menu forwarding
     updateZipUrl: 'https://github.com/Dexsam07/As-zara-mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
